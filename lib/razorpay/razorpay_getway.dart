c1cc8ee8111ebf58e9f5e817546e422aae016d75
Suraj Singh <surajsingh.edugaon@gmail.com>
2023-09-28 09:18:24 +0530

    razorpay integrated

---
+++ lib/razorpay/razorpay_getway.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+class RazorPayPayment extends StatefulWidget {
+  const RazorPayPayment({super.key});
+
+  @override
+  State<RazorPayPayment> createState() => _RazorPayPaymentState();
+}
+
+class _RazorPayPaymentState extends State<RazorPayPayment> {
+
+  Razorpay? _razorpay;
+
+  void _handlePaymentSuccess(PaymentSuccessResponse response){
+  Fluttertoast.showToast(msg: "Success Payment ${response.paymentId}", timeInSecForIosWeb: 4);
+  }
+
+  void _handlePaymentError(PaymentFailureResponse response){
+    Fluttertoast.showToast(msg: "Error Here ${response.code} - ${response.message}", timeInSecForIosWeb: 4);
+  }
+
+  void _handleExternalWallet(ExternalWalletResponse response){
+    Fluttertoast.showToast(msg: "External_Wallet is ${response.walletName}", timeInSecForIosWeb: 4);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _razorpay = Razorpay();
+    _razorpay?.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+    _razorpay?.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+    _razorpay?.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+  }
+
+  void makePayment() async {
+    var options = {
+      'key':'rzp_test_eKeKIpe4Wt30sG',
+      'amount':20000, //Rs = 200
+      'name':'Suraj',
+      'description':'Shoes',
+      'prefill':{'contact':'9162509629','email':'surajsingh.edugaon@gmail.com'},
+    };
+
+    try{
+      _razorpay?.open(options);
+    }
+    catch(e){
+      debugPrint(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("RazorPay"),
+      ),
+      body: ListView(
+        children: [
+          Card(
+            child: ListTile(
+              leading: Image.network("https://w7.pngwing.com/pngs/323/773/png-transparent-sneakers-basketball-shoe-sportswear-nike-shoe-outdoor-shoe-running-sneakers-thumbnail.png"),
+            title: Text("Shoes"),
+              subtitle: Text("Sell Your Kindey and but it now"),
+              trailing: ElevatedButton(onPressed: () {
+                makePayment();
+              }, child: Text("Buy now")),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
